89c80ab00d6552335c4e461711784eb50132fdf3
Tonynovtop <942495626@qq.com>
2021-09-29 21:30:41 +0800

    递归更新

--- get_factorial.py
+++ get_factorial.py
@@ -9,4 +9,29 @@ def get_factorial(n):
     # return result
     return n * get_factorial(n-1)
     
-print(get_factorial(4)) 
+print(get_factorial(4))
+
+def get_factorial1(n):
+    ret = i = 1
+    while i<=n:
+        ret = ret * i
+        i = i + 1
+    return ret
+
+print(get_factorial1(6))
+
+
+# 斐波那契级数，是另一个递归的案例
+def fabnacc(n):
+    # if n == 1:
+    #     return 1
+    # elif n == 2:
+    if n in [1,2]:
+        return 1
+    return fabnacc(n-1) + fabnacc(n-2)
+
+# print(fabnacc(8))
+fabnacc_list = []
+for x in range(1,9):
+    fabnacc_list.append(fabnacc(x))
+print(','.join(str(x) for x in fabnacc_list))
